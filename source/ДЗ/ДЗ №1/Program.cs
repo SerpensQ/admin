a9236e6700eca9307f8ddcd1844998aa8919d560
SerpensQ <serpensq@mail.ru>
2019-11-17 18:51:38 +0500

    Adding a home task again

---
+++ source/ДЗ/ДЗ №1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину прямоугольника");
+            double a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите ширину прямоугольника");
+            double b = Convert.ToInt32(Console.ReadLine());
+            double area = a * b;
+            double perimetr = 2 * a + 2 * b;
+
+            var diagonal = Math.Sqrt(a * a + b * b);
+            Console.WriteLine("Площадь прямоугольника=" + area);
+            Console.WriteLine("Периметр прямоугольника="+perimetr);
+            Console.WriteLine("Диагональ прямоугольника = {0:F3}", diagonal);
+            Console.ReadKey();
+        }
+    }
+}
