791c3a60c4a9e5732fea6ac383d8b1f15a7b3e99
SerpensQ <serpensq@mail.ru>
2019-11-17 19:15:39 +0500

    Adding a home task again

---
+++ source/ДЗ/Логические операции.Формула/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Tasks
+{
+    class Program
+    {
+        static void Main()
+        {
+            var x = F(3, 2) + F(5, 3) + F(11, 7);
+            Console.WriteLine("x={0:F3}", x);
+            Console.ReadKey();
+            
+        }
+        static double F(double x, double y)
+        {
+            return (Math.Sqrt(x) - Math.Sqrt(y) )/ (Math.Sqrt(x) + Math.Sqrt(y));
+        }
+    }
+    
+}
