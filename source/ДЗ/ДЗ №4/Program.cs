2423561262347189a09f9712db3a5cdb8f525a2c
SerpensQ <serpensq@mail.ru>
2019-11-17 19:30:52 +0500

    Adding a home task again

---
+++ source/ДЗ/ДЗ №4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Homework4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите символ.");
+            int symbolCode = Console.Read();
+            Console.WriteLine("0x"+Convert.ToString (symbolCode,16));
+            Console.WriteLine("Следующий символ: {0}", (char)(symbolCode + 1));
+            Console.ReadKey();
+        }
+    }
+}
