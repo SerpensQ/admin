ebef4e74c20e74e2a0963b3b4b00af04acb9a098
SerpensQ <serpensq@mail.ru>
2019-11-17 18:43:43 +0500

    Adding a home task again

---
+++ source/ДЗ/УсловныеОператоры4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace УсловныеОператоры4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение Х");
+            var x = double.Parse(Console.ReadLine());
+            Console.WriteLine(FX(x));
+            Console.ReadKey();
+
+        }
+        static double FX(double x)
+        {
+            if (x > 1)
+                return 2 - x;
+            else if (x <= 1 & x >= -1)
+                return Math.Pow(x, 2);
+            else if (x < -1)
+                return 1;
+            else
+                return 0;
+
+        }
+    }
+}
