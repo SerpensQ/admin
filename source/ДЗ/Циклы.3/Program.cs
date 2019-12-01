d4e02ae05a3898e37a11af2c2f57a35777e28166
SerpensQ <serpensq@mail.ru>
2019-12-01 17:11:55 +0500

    Adding new hometasks

---
+++ source/ДЗ/Циклы.3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace Циклы._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите натуральное число:  ");
+            int number = int.Parse(Console.ReadLine());
+            int i = 1;
+            Console.WriteLine("Числа, квадрат которых меньше " + number);
+            while (Math.Pow(i, 2) < number)
+            {
+                Console.WriteLine(i);
+                i++;
+
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
