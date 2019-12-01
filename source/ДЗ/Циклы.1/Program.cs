b7f7d902bde986420949d4f0ad29673fd7c15ee8
SerpensQ <serpensq@mail.ru>
2019-12-01 17:09:56 +0500

    Adding new hometasks

---
+++ source/ДЗ/Циклы.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Циклы._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.Write("Введите конечное число дюймов:  ");
+            double inch = double.Parse(Console.ReadLine());
+            Console.Write("Введите шаг :  ");
+            double step = double.Parse(Console.ReadLine());
+            for (double i = 1; i <= inch; i += step)
+            {
+                Console.Write(i + "in. = "); Console.Write(i * 2.54 + "cm.\n");
+            }
+           
+            Console.ReadKey();
+
+        }
+
+    }
+
+}
