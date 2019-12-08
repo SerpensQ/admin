bf4d0ca61bda2ad6e8c2d6f871094460beb21fd4
SerpensQ <serpensq@mail.ru>
2019-12-08 19:39:19 +0500

    adding chess

---
+++ source/ДЗ/ChessBoard/ChessBoard/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+
+namespace Chessboard
+{
+    class Program
+    {
+        static void Main()
+        {
+            int boardSize;
+
+            //Здесь ввод размера доски.
+            //оформите его в виде бесконечного цикла с проверкой,
+            //что 1<= boardSize <= 80
+            //и предложением повторного ввода, если условие не выполняется
+            //Предусмотрите отказ от ввода (с последующим завершением программы).
+            
+            PrintBoard(boardSize);           
+
+            Console.ReadKey();
+        }
+
+        static void PrintBoard(int size)
+        {
+            //написать метод печати шахматной доски размера (size x size)
+        }
+
+        /// <summary>
+        /// Печать квадрата нужного цвета
+        /// </summary>
+        /// <param name="color">Цвет квадрата</param>
+        static void PrintSquare(ConsoleColor color)
+        {
+            const char square = (char)0x2588;
+
+            var defaultColor = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.Write(square);
+            Console.ForegroundColor = defaultColor;
+        }
+    }
+}
