190e743a976a564156dea55794f06a23bc057619
SerpensQ <serpensq@mail.ru>
2019-12-15 17:59:47 +0500

    Синусы и косинусы

---
+++ source/ДЗ/ДЗ №0/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Homework2
+{
+    
+    
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                PrintSinCos(13);
+                PrintSinCos(37);
+                PrintSinCos(113);
+                Console.Write("Введите значение угла в градусах ");
+                var angle = double.Parse(Console.ReadLine());
+                PrintSinCos(angle);
+                Console.ReadKey();
+
+                void PrintSinCos(double angleInDegrees)
+                {
+                    double sin = Math.Sin(ConvertDegreesToRadians(angleInDegrees));
+                    double cos = Math.Cos(ConvertDegreesToRadians(angleInDegrees));
+                    Console.WriteLine("Синус угла {0} = {1:0.###}\n Косинус угла {0} = {2:0.###}", angleInDegrees, sin, cos);
+                }
+
+                double ConvertDegreesToRadians(double angleInDeegrees)
+                {
+                    return angleInDeegrees * Math.PI / 180;
+                }
+            }
+        }
+    
+}
