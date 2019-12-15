2384ed6fc6e888b537f338f5eccdbfea2ce9287b
SerpensQ <serpensq@mail.ru>
2019-12-15 17:54:17 +0500

    Исправленная шахматная доска

--- source/ДЗ/ChessBoard/ChessBoard/Program.cs
+++ source/ДЗ/ChessBoard/ChessBoard/Program.cs
@@ -7,28 +7,71 @@ namespace Chessboard
     {
         static void Main()
         {
-            int boardSize;
+            int boardSize = 1;
+            while (true)
+            {
+                Console.WriteLine("Введите размер доски (от 1 до 80). Нажмите Enter для выхода");
+                string str = Console.ReadLine();
+                if (str == string.Empty)
+                    break;
 
-            //Здесь ввод размера доски.
-            //оформите его в виде бесконечного цикла с проверкой,
-            //что 1<= boardSize <= 80
-            //и предложением повторного ввода, если условие не выполняется
-            //Предусмотрите отказ от ввода (с последующим завершением программы).
-            
-            PrintBoard(boardSize);           
+                if (int.TryParse(str, out boardSize))
+                    if (1 <= boardSize && boardSize <= 80)
+                        PrintBoard(boardSize);
+                    else
+                        Console.WriteLine("Размер вне диапазона\n");
+                else
+                {
+                    Console.WriteLine("Ошибка ввода\n");
+                }
+            }
+
+            //Здесь ввод размера доски. 
+            //оформите его в виде бесконечного цикла с проверкой, 
+            //что 1<= boardSize <= 80 
+            //и предложением повторного ввода, если условие не выполняется 
+            //Предусмотрите отказ от ввода (с последующим завершением программы). 
+
+
 
             Console.ReadKey();
         }
 
         static void PrintBoard(int size)
         {
-            //написать метод печати шахматной доски размера (size x size)
+            var board = new bool[size, size];
+            for (var i = 0; i < size; i++)
+                for (var j = 0; j < size; j++)
+                    if ((i + j) % 2 == 0)
+                        board[i, j] = true;
+                    else
+                        board[i, j] = false;
+
+            int counter = 0;
+
+            foreach (var cell in board)
+            {
+                if (cell)
+                    PrintSquare(ConsoleColor.White);
+                else
+                    PrintSquare(ConsoleColor.DarkBlue);
+
+                counter++;
+                if (counter == size)
+                {
+                    counter = 0;
+                    Console.WriteLine();
+                }
+
+            }
+
+            //написать метод печати шахматной доски размера (size x size) 
         }
 
-        /// <summary>
-        /// Печать квадрата нужного цвета
-        /// </summary>
-        /// <param name="color">Цвет квадрата</param>
+        /// <summary> 
+        /// Печать квадрата нужного цвета 
+        /// </summary> 
+        /// <param name="color">Цвет квадрата</param> 
         static void PrintSquare(ConsoleColor color)
         {
             const char square = (char)0x2588;
